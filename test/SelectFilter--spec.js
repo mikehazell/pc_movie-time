c0ea9e6be76e38ba28b646c67eee8e056b81d960
Michael Hazell <mike.hazell@holler.com.au>
2016-06-24 11:29:43 +0200

    TEST: SelectFilter onChange

--- test/SelectFilter--spec.js
+++ test/SelectFilter--spec.js
@@ -1,19 +1,33 @@
 /* eslint-env node, mocha */
 import React from 'react';
 import { expect } from 'chai';
-import { mount } from 'enzyme';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
 import SelectFilter from '../source/components/SelectFilter';
 
+const TEST_TYPE = 'test-type';
 const basicProps = {
     label: 'Testing',
     items: ['one', 'two', 'three'],
-    filterType: 'test',
+    filterType: TEST_TYPE,
     onChange: f => f,
 };
 
 describe('SelectFilter', () => {
     it('renders items as option', () => {
-        const wrapper = mount(<SelectFilter {...basicProps} />);
+        const wrapper = shallow(<SelectFilter {...basicProps} />);
         expect(wrapper.find('option').length).to.equal(basicProps.items.length + 1);
+    });
+    it('should call onChange', () => {
+        const onChange = sinon.spy();
+        const wrapper = shallow(
+            <SelectFilter
+                {...basicProps}
+                onChange={onChange}
+            />
+        );
+        wrapper.find('select').simulate('change', { target: { value: 'test-value' } });
+        expect(onChange.calledOnce).to.equal(true);
+        expect(onChange.calledWith(TEST_TYPE, 'test-value')).to.equal(true);
     });
 });
